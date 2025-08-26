4757e1d78ee215b5f231719f14bc3d461d42f2a3
Jan-Peter Blokdijk <jpblokdijk@hotmail.com>
2025-08-26 21:17:16 +0200

    bestand tijdelijke.py gecodeerd

---
+++ tijdelijke.py
@@ -0,0 +1,18 @@
+mijn_dictionary = {
+    "aardbei": 3,
+    "vanille": 4,
+    "chocolade": 5
+}
+aanbieding = mijn_dictionary["aardbei"] * 0.8
+reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+reclame_tekst2 = reclame_tekst[0:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+for el in reclame_tekst4:
+   for el in reclame_tekst4:
+    print(el.lower())
+for el in reclame_tekst4:
+    if len(el) >= 5:
+        print(el.upper())
+    else:
+        print(el.lower())
